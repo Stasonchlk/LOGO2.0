e59aee73e83473a6c8a94cae26f88afbe5703ced
Stasonchlk <162888827+Stasonchlk@users.noreply.github.com>
2024-10-28 19:19:41 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,14 @@
+// Конфигурация Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAhJwmFlIgXt0z6gcoErRTXQo3kkjDqOuo",
+  authDomain: "lastp-44a37.firebaseapp.com",
+  databaseURL: "https://lastp-44a37-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "lastp-44a37",
+  storageBucket: "lastp-44a37.appspot.com",
+  messagingSenderId: "972643290372",
+  appId: "1:972643290372:web:c42fdc293cf8cac0609451",
+  measurementId: "G-C7R84JG5XZ"
+};
 
+// Инициализация Firebase
+firebase.initializeApp(firebaseConfig);
